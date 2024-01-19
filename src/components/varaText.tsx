20b2f6ef3a87b64615dcc6edcbeece4efe67a427
Alex Rigl <crigl@chapman.edu>
2024-01-19 15:01:01 -0700

    adjusted font size of varatext for mobile devices

--- src/components/varaText.tsx
+++ src/components/varaText.tsx
@@ -1,21 +1,23 @@
-import {useEffect } from 'react';
-import Vara from 'vara';
+import { useEffect } from "react";
+import Vara from "vara";
 
 function VaraText({ text }: { text: string }) {
-  const containerId = `vara-container-${text.replace(/\s/g, '')}`;
-  console.log("testing")
+  const containerId = `vara-container-${text.replace(/\s/g, "")}`;
+  console.log("testing");
   useEffect(() => {
-    console.log(sessionStorage.getItem('animationTriggered'))
-    console.log("vara text")
+    console.log(sessionStorage.getItem("animationTriggered"));
+    console.log("vara text");
     const container = document.getElementById(containerId);
 
     if (container) {
       // If the container exists, clear its content
-      console.log(container.innerHTML)
-      container.innerHTML = '';
+      console.log(container.innerHTML);
+      container.innerHTML = "";
+
+      const fontSize = window.innerWidth < 768 ? 40 : 50;
 
       // Initialize Vara only if the animation has not been triggered in the current session
-      const animationTriggered = sessionStorage.getItem('animationTriggered');
+      const animationTriggered = sessionStorage.getItem("animationTriggered");
       if (!animationTriggered) {
         new Vara(
           `#${containerId}`,
@@ -23,41 +25,40 @@ function VaraText({ text }: { text: string }) {
           [
             {
               text: text,
-              fontSize: 50,
+              fontSize: fontSize,
               strokeWidth: 1.5,
               delay: 1000,
               textAlign: "center",
-              color: "black"
+              color: "black",
             },
           ]
-        )
+        );
 
         // Set the flag in sessionStorage to indicate that the animation has been triggered in this session
-        sessionStorage.setItem('animationTriggered', 'true');
-      } else{
+        sessionStorage.setItem("animationTriggered", "true");
+      } else {
         new Vara(
           `#${containerId}`,
           "https://raw.githubusercontent.com/akzhy/Vara/master/fonts/Satisfy/SatisfySL.json",
           [
             {
               text: text,
-              fontSize: 50,
+              fontSize: fontSize,
               strokeWidth: 1.5,
               delay: 0,
               textAlign: "center",
               color: "black",
-              autoAnimation:true
+              autoAnimation: true,
             },
           ]
-        )
+        );
       }
     }
   }, [containerId, text]);
 
-  return <div id={containerId} className=''></div>;
+  return <div id={containerId} className=""></div>;
 }
 
 // Rest of your component...
-
 
 export default VaraText;
