b7d6352660205149d3caf8d2678396d18f445489
Alex Rigl <crigl@chapman.edu>
2024-01-24 12:03:16 -0700

    project popup for mobile

--- src/components/subcomponents/projectPopup.tsx
+++ src/components/subcomponents/projectPopup.tsx
@@ -1,13 +1,12 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import { Badge } from "@/components/ui/badge"
+import { Badge } from "@/components/ui/badge";
 // import IMAGES from '@/images/IMAGES';
-import projectData from '../data/projectData';
+import projectData from "../data/projectData";
 // import IMAGES from '../data/images';
 
-import { Separator } from "@/components/ui/separator"
-import Carousel from './Carousel';
-
+import { Separator } from "@/components/ui/separator";
+import Carousel from "./Carousel";
 
 function ProjectPopup(props: {
   projectName: string;
@@ -22,19 +21,19 @@ function ProjectPopup(props: {
   setShowPopup: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
   const setPrevProject = () => {
-    if(props.currentProject != 0){
-      props.setCurrentProject(prev => prev-1)
+    if (props.currentProject != 0) {
+      props.setCurrentProject((prev) => prev - 1);
     }
   };
   const setNextProject = () => {
-    if(props.currentProject < projectData.length-1){
-      props.setCurrentProject(prev => prev+1)
+    if (props.currentProject < projectData.length - 1) {
+      props.setCurrentProject((prev) => prev + 1);
     }
   };
   const closePopup = () => {
     console.log("close popup");
     setIsClosing(true);
-  
+
     // Introduce a delay before setting isVisible to false
     setTimeout(() => {
       setIsVisible(false);
@@ -66,69 +65,107 @@ function ProjectPopup(props: {
 
   useEffect(() => {
     // Disable scrolling when the popup is open
-    document.body.style.overflow = 'hidden';
+    document.body.style.overflow = "hidden";
 
     // Enable scrolling when the popup is closed
     return () => {
-      document.body.style.overflow = 'auto';
+      document.body.style.overflow = "auto";
     };
   }, [props.showPopup, isVisible]);
 
   //console.log(projectData[props.currentProject].slideImages);
 
-
   return (
     <div>
       {props.showPopup && (
-        <div className={`fixed bottom-0 left-0 w-screen h-screen flex flex-col bg-white bg-opacity-100 transition-transform duration-1000 ease-in-out p-20 ${isVisible ? 'translate-y-0' : 'translate-y-full'}`}>
-            <div className="flex flex-col">
-              <div className='flex flex-row gap-10'> 
-                  <div className='w-1/2	'>
-                    <Carousel currentProject={props.currentProject} />
-                  </div>
-                  <div className='flex flex-col justify-between'>
-                      <div className='text-left flex-grow'>
-                          <h1 className='text-xs items-center font-mono font-medium text-orange-500'>Project Name</h1>
-                          <h1>{projectData[props.currentProject].projectName}</h1>
-                          <h1 className='text-xs items-center font-mono font-medium text-orange-500 pt-2'>Description</h1>
-                          <p>{projectData[props.currentProject].description}</p>
-                          <h1 className='flex gap-1.5 text-xs items-center font-mono font-medium text-orange-500 pt-2'>Tools</h1>
-                          {Array.from({ length: props.frameworks.length }).map((_, index) => (
-                               <Badge key={index} variant="outline" className='bg-black text-white mr-2 mb-2 text-white'>{props.frameworks[index]}</Badge>
-                          ))}
-                          {props.website != "" && (
-                            <div>
-                              <h1 className='flex gap-1.5 text-xs items-center font-mono font-medium text-orange-500 pt-2'>View Live</h1>
-                              <p>{props.website}</p>
-                            </div>
-                          )}
-                      </div>
-                      
-                      <button onClick={closePopup} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
-                          Close
-                      </button>
-                  </div>
+        <div
+          className={`fixed bottom-0 left-0 w-screen h-screen flex flex-col bg-white bg-opacity-100 transition-transform duration-1000 ease-in-out md:p-20 ${
+            isVisible ? "translate-y-0" : "translate-y-full"
+          }`}
+        >
+          <div className="flex flex-col">
+            <div className="flex flex-col gap-10 md:flex-row">
+              <div className="w-screen md:w-1/2	">
+                <Carousel currentProject={props.currentProject} />
               </div>
-              <div className='flex flex-col justify-between h-max mb-10 ml-10 mr-10'>
-                  <Separator className='mt-20 mb-5'></Separator>
-                  <div className='flex flex-row justify-between'>
-                      {props.currentProject == 0 && (
-                        <button className="bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded ml-2 opacity-50">Previous Project</button>
-                      )}
-                      {props.currentProject != 0 && (
-                        <button onClick={setPrevProject} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded ml-2">Previous Project</button>
-                      )}
-                      {props.currentProject == projectData.length - 1 && (
-                        <button onClick={setNextProject} className="bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded mr-2 opacity-50">Next Project</button>
-                      )}
-                      {props.currentProject != projectData.length - 1 && (
-                        <button onClick={setNextProject} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded mr-2">Next Project</button>
-                      )}
-                      {/* <button onClick={setPrevProject} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded ml-2">Previous Project</button> */}
-                      {/* <button onClick={setNextProject} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded mr-2">Next Project</button> */}
-                  </div>
-              </div> 
-            
+              <div className="flex flex-col justify-between">
+                <div className="text-left flex-grow">
+                  <h1 className="text-xs items-center font-mono font-medium text-orange-500">
+                    Project Name
+                  </h1>
+                  <h1>{projectData[props.currentProject].projectName}</h1>
+                  <h1 className="text-xs items-center font-mono font-medium text-orange-500 pt-2">
+                    Description
+                  </h1>
+                  <p>{projectData[props.currentProject].description}</p>
+                  <h1 className="flex gap-1.5 text-xs items-center font-mono font-medium text-orange-500 pt-2">
+                    Tools
+                  </h1>
+                  {Array.from({ length: props.frameworks.length }).map(
+                    (_, index) => (
+                      <Badge
+                        key={index}
+                        variant="outline"
+                        className="bg-black text-white mr-2 mb-2 text-white"
+                      >
+                        {props.frameworks[index]}
+                      </Badge>
+                    )
+                  )}
+                  {props.website != "" && (
+                    <div>
+                      <h1 className="flex gap-1.5 text-xs items-center font-mono font-medium text-orange-500 pt-2">
+                        View Live
+                      </h1>
+                      <p>{props.website}</p>
+                    </div>
+                  )}
+                </div>
+
+                <button
+                  onClick={closePopup}
+                  className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-col justify-between h-max mb-10 ml-10 mr-10">
+              <Separator className="mt-20 mb-5"></Separator>
+              <div className="flex flex-row justify-between">
+                {props.currentProject == 0 && (
+                  <button className="bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded ml-2 opacity-50">
+                    Previous Project
+                  </button>
+                )}
+                {props.currentProject != 0 && (
+                  <button
+                    onClick={setPrevProject}
+                    className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded ml-2"
+                  >
+                    Previous Project
+                  </button>
+                )}
+                {props.currentProject == projectData.length - 1 && (
+                  <button
+                    onClick={setNextProject}
+                    className="bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded mr-2 opacity-50"
+                  >
+                    Next Project
+                  </button>
+                )}
+                {props.currentProject != projectData.length - 1 && (
+                  <button
+                    onClick={setNextProject}
+                    className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded mr-2"
+                  >
+                    Next Project
+                  </button>
+                )}
+                {/* <button onClick={setPrevProject} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded ml-2">Previous Project</button> */}
+                {/* <button onClick={setNextProject} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded mr-2">Next Project</button> */}
+              </div>
+            </div>
           </div>
         </div>
       )}
